23b9e1e58b4f3acc1441c1d6fe6c3b7bb44e3637
DRCcodemaster <93834420+DRCcodemaster@users.noreply.github.com>
2022-04-04 21:59:00 -0500

    My-Projects

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string item;
+	double price;
+	int quantity;
+	double total;
+
+	cout << "What item do you want to buy?";
+	getline(cin, item);
+	cout << "What is the price for each? $";
+	cin >> price;
+	cout << "How many do you want to buy?: ";
+	cin >> quantity;
+	cout << endl;
+
+	total = price * quantity;
+
+	cout << " item:" << item << endl;
+	cout << " price:" << price << endl;
+	cout << "X" << quantity << endl;
+	cout << " Your total is: $" << total << endl;
+
+	return 0;
+
+}
